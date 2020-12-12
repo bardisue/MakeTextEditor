3f25d5c8ca2d4273cc2be5d707461414bc84cad3
tr0568@o.cnu.ac.kr <tr0568@o.cnu.ac.kr>
2020-12-13 08:58:03 +0900

    체인지까지 구현

--- MakeTextEditor/test.cpp
+++ MakeTextEditor/test.cpp
@@ -7,7 +7,7 @@ int main() {
 	vec1.push_back(*new vector<int>);
 	vec1[0].push_back(3);
 	vec1.push_back(*new vector<int>);
-	vec1[1].push_back(4);
+	vec1[0].push_back(3);
 	for (int i = 0; i < vec1.size(); i++) {
 		for (int j = 0; j < vec1[0].size(); j++) {
 			cout << vec1[i][j] << endl;
--- MakeTextEditor/texteditor.cpp
+++ MakeTextEditor/texteditor.cpp
@@ -53,10 +53,14 @@ void makeConsole(string message) {
 }
 
 void showContext(int start, int end) {
+    int line = 0;
     for (int i = start; i < end; i++) {
+        if (fullContents[i].size() == 0) {
+            continue;
+        }
         cout.width(2);
         cout.fill(' ');
-        cout << i + 1 << "| ";
+        cout << ++line << "| ";
         for (int j = 0; j < fullContents[i].size(); j++)
         {
             cout << fullContents[i][j];
@@ -73,10 +77,11 @@ int checkBytes(int index) {
     return bytes;
 }
 
-void makeSF(int start) {
+void makeSFI(int start) {
+    string toInsert;
     for (int i = start; i < fullContents.size(); i++) {
         while (checkBytes(i) > 76) {
-            string toInsert = fullContents[i][fullContents[i].size() - 1];
+            toInsert = fullContents[i][fullContents[i].size() - 1];
             fullContents[i].pop_back();
             if (i == fullContents.size()-1) {
                 fullContents.push_back(*new vector<string>);
@@ -84,6 +89,37 @@ void makeSF(int start) {
             fullContents[i + 1].insert(fullContents[i + 1].begin(), toInsert);
         }
     }
+}
+
+void makeSFD(int start) {
+    string toInsert;
+    for (int i = start; i < fullContents.size(); i++) {
+        while (checkBytes(i) <= 76) {
+            if (i == (fullContents.size()-1)) {
+                break;
+            }
+            if (fullContents[i + 1].size() == 0) {
+                break;
+            }
+            else if (checkBytes(i) + fullContents[i + 1][0].size() <= 76) {
+                toInsert = fullContents[i + 1][0];
+                fullContents[i + 1].erase(fullContents[i + 1].begin());
+                fullContents[i].push_back(toInsert);
+            }
+            else {
+                break;
+            }
+        }
+    }
+}
+
+bool isInt(string toCheck) {
+    for (int i = 0; i < toCheck.length(); i++) {
+        if (57 <(int)toCheck[i] || (int)toCheck[i] < 48) {
+            return false;
+        }
+    }
+    return true;
 }
 
 class Default : public excuteStrategy {
@@ -169,23 +205,115 @@ class Insert : public excuteStrategy {
         while (getline(ss, stringBuffer, ',')) {
             atribute.push_back(stringBuffer);
         }
-        int insertPage = firstPage + atoi(atribute[0].c_str())-1;
-        int insertPoisition = atoi(atribute[1].c_str());
-        string insertWord = atribute[2] + " ";
-        fullContents[insertPage].insert(fullContents[insertPage].begin()+ insertPoisition, insertWord);
-        makeSF(insertPage);
-        showContext(firstPage, lastPage);
-        makeConsole("마지막 페이지입니다");
+        if (atribute.size() != 3) {
+            showContext(firstPage, lastPage);
+            makeConsole("요소의 개수를 확인해주십시오");
+            return;
+        }
+        if (isInt(atribute[0]) && isInt(atribute[1])){
+            int insertPage = firstPage + atoi(atribute[0].c_str()) - 1;
+            int insertPoisition = atoi(atribute[1].c_str());
+            string insertWord = atribute[2] + " ";
+            if (insertWord.length() > 75) {
+                showContext(firstPage, lastPage);
+                makeConsole("75자 이하의 단어를 넣어주세요");
+                return;
+            }
+            if ((int)atoi(atribute[0].c_str()) > (int)20 || (int)atoi(atribute[0].c_str()) < (int)0) {
+                showContext(firstPage, lastPage);
+                makeConsole("존재하지 않는 line입니다");
+                return;
+            }
+
+            if ((int)insertPoisition<0 || (int)insertPoisition > fullContents[insertPage].size()) {
+                showContext(firstPage, lastPage);
+                makeConsole("해당라인에 존재하지 않는 위치입니다.");
+                return;
+            }
+
+            fullContents[insertPage].insert(fullContents[insertPage].begin() + insertPoisition, insertWord);
+            makeSFI(insertPage);
+            showContext(firstPage, lastPage);
+            makeConsole("(입력메세지)");
+        }
+        else {
+            showContext(firstPage, lastPage);
+            makeConsole("정수만 입력해주십시오");
+        }
     }
 };
 class Delete : public excuteStrategy {
     void excute() {
+        system("cls");
+        istringstream ss(strategy);
+        string stringBuffer;
+        vector<string> atribute;
+        atribute.clear();
+        while (getline(ss, stringBuffer, ',')) {
+            atribute.push_back(stringBuffer);
+        }
+        if (atribute.size() != 2) {
+            showContext(firstPage, lastPage);
+            makeConsole("요소의 개수를 확인해주십시오");
+            return;
+        }
+        if (isInt(atribute[0]) && isInt(atribute[1])) {
+            int insertPage = firstPage + atoi(atribute[0].c_str()) - 1;
+            int insertPoisition = atoi(atribute[1].c_str())-1;
+            if (fullContents[insertPage].size() == 0 || (int)atoi(atribute[0].c_str()) < (int)0) {
+                showContext(firstPage, lastPage);
+                makeConsole("존재하지 않는 line입니다");
+                return;
+            }
+            if ((int)insertPoisition<0 || (int)insertPoisition >= fullContents[insertPage].size()) {
+                showContext(firstPage, lastPage);
+                makeConsole("해당라인에 존재하지 않는 위치입니다.");
+                return;
+            }
 
+            fullContents[insertPage].erase(fullContents[insertPage].begin() + insertPoisition);
+            makeSFD(insertPage);
+            showContext(firstPage, lastPage);
+            makeConsole("(입력메세지)");
+        }
+        else {
+            showContext(firstPage, lastPage);
+            makeConsole("정수만 입력해주십시오");
+        }
     }
 };
 class Change : public excuteStrategy {
     void excute() {
-
+        system("cls");
+        istringstream ss(strategy);
+        string stringBuffer;
+        vector<string> atribute;
+        atribute.clear();
+        while (getline(ss, stringBuffer, ',')) {
+            atribute.push_back(stringBuffer);
+        }
+        if (atribute.size() != 2) {
+            showContext(firstPage, lastPage);
+            makeConsole("요소의 개수를 확인해주십시오");
+            return;
+        }
+        while (getline(ss, stringBuffer, ',')) {
+            atribute.push_back(stringBuffer);
+        }
+        for (int i = 0; i < fullContents.size(); i++) {
+            for (int j = 0; j < fullContents[i].size(); j++) {
+                if (fullContents[j].size() == 0) {
+                    break;
+                }
+                if (fullContents[i][j] == atribute[0]+ " ") {
+                    fullContents[i][j] = atribute[1] + " ";
+                }
+            }
+        }
+        makeSFD(0);
+        makeSFI(0);
+        showContext(firstPage, lastPage);
+        makeConsole("(입력메세지)");
     }
 };
 class Search : public excuteStrategy {
@@ -225,6 +353,16 @@ int main() {
         case 'i':
             strategy = strategy.substr(2, strategy.size() - 3);
             texteditor->setExcuteStrategy(insert);
+            texteditor->excute();
+            break;
+        case 'd':
+            strategy = strategy.substr(2, strategy.size() - 3);
+            texteditor->setExcuteStrategy(delete1);
+            texteditor->excute();
+            break;
+        case 'c':
+            strategy = strategy.substr(2, strategy.size() - 3);
+            texteditor->setExcuteStrategy(change);
             texteditor->excute();
             break;
         default:
